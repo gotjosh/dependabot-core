gocardless/bump-core

Add FileFetchers::Elixir::Hex

lib/bump/file_fetchers.rb
@@ -2,6 +2,7 @@
 require "bump/file_fetchers/ruby/bundler"
 require "bump/file_fetchers/python/pip"
 require "bump/file_fetchers/java_script/yarn"
+require "bump/file_fetchers/elixir/hex"
 
 module Bump
   module FileFetchers
@@ -10,6 +11,7 @@ module Bump
       when "bundler" then FileFetchers::Ruby::Bundler
       when "yarn" then FileFetchers::JavaScript::Yarn
       when "pip" then FileFetchers::Python::Pip
+      when "hex_elixir" then FileFetchers::Elixir::Hex
       else raise "Unsupported package_manager #{package_manager}"
       end
     end

gotjosh/dependabot-core

lib/dependabot/file_fetchers.rb
@@ -3,6 +3,7 @@ require "dependabot/file_fetchers/ruby/bundler"
 require "dependabot/file_fetchers/python/pip"
 require "dependabot/file_fetchers/java_script/yarn"
 require "dependabot/file_fetchers/php/composer"
+require "dependabot/file_fetchers/elixir/hex"
 
 module Dependabot
   module FileFetchers
@@ -12,6 +13,7 @@ module Dependabot
       when "yarn" then FileFetchers::JavaScript::Yarn
       when "pip" then FileFetchers::Python::Pip
       when "composer" then FileFetchers::Php::Composer
+      when "hex" then FileFetchers::Elixir::Hex
       else raise "Unsupported package_manager #{package_manager}"
       end
     end